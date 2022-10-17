43e9c661761718101c1b34336aa423da4e1be7cc
uranjan445 <58565275+uranjan445@users.noreply.github.com>
2022-10-17 22:28:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
+
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+
+  const [queryText , setqueryText] = useState("");
+  const [data , setData] = useState(null);
+  const inputHandler = (event) =>{
+    
+    setqueryText(event.target.value);
+    
+    if(queryText.length >0){
+    const queryString = `https://61824bbb84c2020017d89da4.mockapi.io/api/v1/names?name=${queryText}`;
+    const newData  = axios.get(queryString).then((res) => res.data);
+    
+    newData.then((resolve,reject)=>{
+      setData(resolve);
+      // console.log(data.items);
+      })
+    }
+  }
+
+  const copyInputHandler = (eachItemName) =>{
+    setqueryText(eachItemName);
+    setData(null);
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='appName'>Name Search
+      <input type="text" className='searchBox'  onChange ={inputHandler} value = {queryText}></input>
+      <div className='dataItems'>
+        { queryText.length > 0 && data && (
+          data.items.map((eachItem)=>{
+          return (
+            <p onClick={() => copyInputHandler(eachItem.name)} >{eachItem.name}</p>
+          )
+        }))
+
+      } 
+      </div>
+    </div>
     </div>
   );
 }
